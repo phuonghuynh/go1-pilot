89ce43111ddfe3ae146260e7b64f637c323b256d
phuonghqh <phuonghqh@gmail.com>
2017-06-06 12:06:45 +0700

    Use PhantomJS instead of Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
         singleRun: false
     });
 };
